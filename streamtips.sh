d901ab7f7bd01c8343157ec87548fb0e546b39e3
jess <jkant@bu.edu>
2026-01-07 01:42:00 -0500

    fixed default URL for stream enumerator

--- streamtips.sh
+++ streamtips.sh
@@ -92,13 +92,24 @@ ffmpeg -f concat -i temp -c copy "${outNAME}_all.mp4" && rm temp &&
 ffmpeg -i "${outNAME}_all.mp4" -vn -ac 2 -b:a 192k "${outNAME}_all.mp3"
 
 # for rhode island
-
+# RI Capital TV loads *all* media files into source
 curl $targ |  tr "\"" "\n" | grep  "\Khttps.*?1080.*?m3u?8" -Poz -m 1
 
-# stream counter
+# stream counter based on RI capital TV
+# page lists all media content in JSON in source
+default_URL="capitoltvri.cablecast.tv/watch/stream/1"
+if ! [[ -v targ &&  "${#targ}" -gt 0 ]]; then
+	targ=$default_URL
+	printf "\n\t"; else printf "\n\t $(echo "[ current  URL: "  $targ "]") \n\t" ;
+fi
+read -p " -> Target (url): " targ
+targ="${targ:=$default_URL}"
 curl $targ |  tr "\"" "\n" > counter_temp
 	sed 's/,/\n/g' counter_temp | grep '^https.*?m3u?8$' -E | \
 	printf "\n\tThere are $(wc -l) m3u/m3u8 streams\n" &&
-	grep -P "http" counter_temp | grep "mp4$" |
-		printf "\t$(wc -l) mp4 files found\n" &&
-		printf "\t$(grep -Ec '.pdf$' counter_temp) pdf files found.\n\n"
+	grep -P "http" counter_temp |
+		printf "\t$(grep "mp4$" -c) mp4 files found\n" &&
+		printf "\t$(grep -Ec '.pdf$' counter_temp) pdf files found.\n\n\tlivestreams:\n\n" &&
+		sed  's/\\//g' counter_temp | grep "http.*live?stream=[0-9+]" -o |
+			sort | uniq | printf "$(grep -Po "(?s)https:/\/\\K.*" |
+			sed 's/^/\t-> /g')\n\n"
